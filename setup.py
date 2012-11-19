cd138b7349d3b957e2668ed34ab9b80f264d9006
Alex Clark <aclark@aclark.net>
2012-11-19 12:40:29 -0500

    Add entry point

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
+    entry_points={
+        'z3c.autoinclude.plugin': 'target=plone',
+    },
     name='plone_addon_upgrade',
     packages=find_packages(),
 )
